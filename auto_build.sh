55187433ef96c457fd51b876e98afed31a2aad2a
terryyu <tunghuayu@gingytech.com.tw>
2022-02-11 17:41:40 +0800

    Add CMake install feature
    - edit cmake/install.cmake to implement install feature
    - add auto_build.sh to automatically do building work.

---
+++ auto_build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# check if build folder exist
+if [[ -d build ]]; then
+    echo "build/ folder already exist"
+    printf "%s " "overwrite? (yes/no) "
+    read ans
+    if [[ ${ans} == "yes" ]]; then
+        rm build/ -rf
+    else 
+        exit 0
+    fi
+fi
+
+# create build folder
+mkdir build
+
+# record current work path
+origin_pwd=$(pwd)
+
+# go to build folder
+cd build
+
+# cmake 
+cmake .. \
+-D CMAKE_INSTALL_PREFIX=$(pwd)/install \
+-D CMAKE_BUILD_TYPE=Release
+
+make 
+
+make install
+
+# ctest
---
+++ cmake/install.cmake
@@ -0,0 +1,28 @@
+# import GNUInstallDirs cmake module
+include(GNUInstallDirs)
+
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR})
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_BINDIR})
+
+
+# <<< Install and export targets >>>
+    
+install(
+    TARGETS
+        add-objs
+        add-shared
+        add-static
+    ARCHIVE
+        DESTINATION ${INSTALL_LIBDIR}
+        COMPONENT lib
+    RUNTIME
+        DESTINATION ${INSTALL_BINDIR}
+        COMPONENT bin
+    LIBRARY
+        DESTINATION ${INSTALL_LIBDIR}
+        COMPONENT lib
+    PUBLIC_HEADER
+        DESTINATION ${INSTALL_INCLUDEDIR}
+        COMPONENT dev
+)
